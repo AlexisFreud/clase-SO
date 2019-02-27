e81e5c21cadde4e2686220cd34a1b4789e552e6c
Alexis <A01631994@itesm.mx>
2019-02-27 02:06:07 +0000

    practica3

--- init.c
+++ init.c
@@ -20,7 +20,7 @@ main(void)
   dup(0);  // stderr
 
   for(;;){
-    printf(1, "init: starting sh\n");
+    printf(1, "\n\n******************************************************\nBienvenido a tu super genial ultra gay sistema operativo \n#FactorTec \n#YoSoyJuan\n******************************************************\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
